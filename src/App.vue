acc520c8f6836cbb7a73f63d61bd7e7dab44ef80
Sarah Martinelli <s-smb@hotmail.com>
2020-02-24 18:57:04 -0500

    fix property name

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
       >
         <img v-if="loading" class="self-center" src="@/assets/loading.gif" />
         <div
-          v-else-if="properties"
+          v-else-if="filteredPropertiesByAmenities"
           class="grid grid-flow-row grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 xl:gap-12 "
         >
           <Card
